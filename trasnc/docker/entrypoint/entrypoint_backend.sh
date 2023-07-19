daae585464f4b1685d24f3a5d2d65622e963f81b
Romain Cuminal <rcuminal@z4r2p5.42lyon.fr>
2023-07-19 22:06:21 +0200

    maj

--- trasnc/docker/entrypoint/entrypoint_backend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ ${ENV} = "DEV" ]; then 
-	yarn run start:dev
-else
-	yarn run build
-	node dist/src/main
-fi
--- trasnc/docker/entrypoint/entrypoint_frontend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ ${ENV} = "DEV" ]; then 
-	yarn run dev
-else
-	yarn run build
-	yarn global add serve
-	serve -s build
-fi
